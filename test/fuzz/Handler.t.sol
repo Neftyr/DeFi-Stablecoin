ebac2117f1589daad96647645b411de5a0b55df4
Niferu <wargacki.patryq@gmail.com>
2023-09-02 19:51:52 +0200

    unused contracts removed

--- test/fuzz/Handler.t.sol
+++
@@ -1,5 +0,0 @@
-/** @dev Handler is going to narrow down the way we call functions (this way we do not waste runs) */
-
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
--- test/fuzz/InvariantsTest.t.sol
+++
@@ -1,5 +0,0 @@
-/** @dev This will have our invariant aka properties of system that should always holds */
-
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
